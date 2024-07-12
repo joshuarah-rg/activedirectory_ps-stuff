aa3f8942e360c919f7c727688f305d7074f0c68c
rahmlow <joshua.rahmlow@gmail.com>
2024-07-12 13:20:26 +0200

    Ausweisung der OU als Variable zur besseren Sichtbarkeit

--- forcePasswordChangeAfterLogon.ps1
+++ forcePasswordChangeAfterLogon.ps1
@@ -1,1 +1,6 @@
-Get-ADUser -Filter * -SearchBase "OU" | Set-ADUser -Enabled $true -ChangePasswordAtLogon $true
+# Aktiviert ALLE Benutzerkonten einer OU
+# Verpflichtet die Nutzer jener OU dazu ihr Passwort zu ändern.
+
+$organizationalUnit = "Distinguished Name" # Hier den Distinguished Name der OU angeben
+
+Get-ADUser -Filter * -SearchBase $organizationalUnit | Set-ADUser -Enabled $true -ChangePasswordAtLogon $true
