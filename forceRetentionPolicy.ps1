d02545185c77e9a8a712f7106ad9a921ef7f36b3
rahmlow <joshua.rahmlow@gmail.com>
2024-07-15 09:02:10 +0200

    utf zeug

--- forceRetentionPolicy.ps1
+++ forceRetentionPolicy.ps1
@@ -1,5 +1,5 @@
-$Postfächer = Get-Mailbox -ResultSize Unlimited
-foreach ($Postfach in $Postfächer) {
+$Postfaecher = Get-Mailbox -ResultSize Unlimited
+foreach ($Postfach in $Postfaecher) {
     $Richtlinie = Get-RetentionPolicy | Where-Object {$_.Mailbox -eq $Postfach.Name}
     if (-not $Richtlinie) {
         Set-Mailbox -Identity $Postfach.Name -RetentionPolicy "Default Archive and Retention Policy"
